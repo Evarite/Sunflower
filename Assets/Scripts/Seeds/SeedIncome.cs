7fbaa637b05f76261d7b1134e50333db1a522efb
pixaut <116499977+pixaut@users.noreply.github.com>
2026-08-08 17:05:41 +0300

    Economy system start editing (#28)

---
+++ Assets/Scripts/Seeds/SeedIncome.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using System.Collections;
+
+namespace Sunflower.Seeds
+{
+    
+    public class SeedsIncome: MonoBehaviour
+    {
+        
+        [SerializeField]
+        private float _timeInterval = 2;
+        [SerializeField]
+        private float _seedsVelocity = 10;
+
+
+        private WaitForSeconds _seedsInterval;
+
+        private void Awake() {
+            _seedsInterval = new WaitForSeconds(_timeInterval);
+        }
+
+        private void OnEnable()
+        {
+            Managers.LoseManager.Lost += StopSeedsUpdate;
+            StartCoroutine(UpdateSeedsValue());
+        }
+        private void OnDisable()
+        {
+            Managers.LoseManager.Lost -= StopSeedsUpdate;
+            StopAllCoroutines();
+        }
+
+        private void StopSeedsUpdate()
+        {
+            gameObject.SetActive(false);
+            //To Do: добавить включение по ивенту в рестарте
+        }
+
+        
+        private IEnumerator UpdateSeedsValue()
+        {
+            
+            while (true)
+            {
+                SeedsCounter.Value += (int)( _seedsVelocity * _timeInterval);
+                yield return _seedsInterval;
+            }
+        }
+
+
+    }
+
+}
