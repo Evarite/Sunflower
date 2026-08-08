82a34a73232f115899ebfc2dff335ba567ac9905
Alaksiej <forestingel@gmail.com>
2026-08-08 10:56:59 +0200

    NeedUI created.

--- Assets/Scripts/Needs/Need.cs
+++ Assets/Scripts/Needs/Need.cs
@@ -6,6 +6,16 @@ namespace Sunflower.Needs
     {
         private float _value = 0f;
 
-        public float Value { get => _value; set => _value = value; }
+        public float Value
+        {
+            get => _value;
+            set
+            {
+                _value = Mathf.Clamp01(value);
+                OnValueChanged?.Invoke(_value);
+            }
+        }
+
+        public event System.Action<float> OnValueChanged;
     }
 }
---
+++ Assets/Scripts/Needs/NeedUI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Sunflower.Needs
+{
+    [RequireComponent(typeof(Slider))]
+    public class NeedUI : MonoBehaviour
+    {
+        [SerializeField] private Need _need;
+
+        private Slider _slider;
+
+        private void Awake() => _slider = GetComponent<Slider>();
+
+        private void OnEnable() => _need.OnValueChanged += OnValueChanged;
+
+        private void OnDisable() => _need.OnValueChanged -= OnValueChanged;
+
+        private void OnValueChanged(float value) => _slider.value = value;
+    }
+}
