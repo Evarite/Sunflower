6e6a7ee4e6f8b1cd0c4ab58e2da3c1dbf16b1fe6
Alaksiej <forestingel@gmail.com>
2026-08-09 11:58:59 +0200

    Skills created. Load system created.

---
+++ Assets/Scripts/SkillTree/Skills/WaterSkill.cs
@@ -0,0 +1,16 @@
+﻿using Sunflower.SkillTree.Data;
+using UnityEngine;
+
+namespace Sunflower.SkillTree.Skills
+{
+    [AddComponentMenu("Sunflower/Skills/Water Skill")]
+    public class WaterSkill : Skill
+    {
+        private void Awake() => Id = SkillId.WaterSkill;
+
+        protected override void Ability()
+        {
+
+        }
+    }
+}
