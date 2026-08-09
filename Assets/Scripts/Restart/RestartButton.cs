cd45b0144d460d88113b06cf5ff7ec37df34ec04
Alaksiej <forestingel@gmail.com>
2026-08-09 13:08:29 +0200

    RestartButton scripts created.

---
+++ Assets/Scripts/Restart/RestartButton.cs
@@ -0,0 +1,30 @@
+using Sunflower.Growth;
+using Sunflower.Managers;
+using Sunflower.SkillTree.EvolutionPoints;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Sunflower.Restart
+{
+    [RequireComponent(typeof(Button))]
+    public class RestartButton : MonoBehaviour
+    {
+        [SerializeField] private SunflowerGrowth _sunflowerGrowth;
+
+        private Button _button;
+
+        private void Awake() => _button = GetComponent<Button>();
+
+        private void OnEnable() => _button.onClick.AddListener(RestartGame);
+
+        private void OnDisable() => _button.onClick.RemoveListener(RestartGame);
+
+        private void RestartGame()
+        {
+            int points = CalculateEvoPoints.Calculate(_sunflowerGrowth.Height);
+            PlayerStateManager.Instance.PointsEarned = points;
+
+            EvoPointsCounter.Value += points;
+        }
+    }
+}
