15c27b416475eca69d10a7f5ea109225fcf4271b
pixaut <116499977+pixaut@users.noreply.github.com>
2026-08-09 16:03:36 +0300

    Add bee (#41)

---
+++ Assets/Scripts/Enemies/BeeEnemy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sunflower.Enemies;
+
+namespace Sunflower.Enemies
+{
+    class BeeEnemy : Enemy
+    {
+        protected override void AfterAttack(ITargetable target)
+        {
+            base.AfterAttack(target);
+
+            Die();
+        }
+    }
+}
--- Assets/Scripts/Enemies/Data/PassiveEnemyData.cs
+++ Assets/Scripts/Enemies/Data/PassiveEnemyData.cs
@@ -2,10 +2,13 @@
 
 namespace Sunflower.Enemies
 {
-    [CreateAssetMenu(fileName = "PassiveEnemy", menuName = "Sunflower/Passive Enemy")]
+    [CreateAssetMenu(fileName = "PassiveEnemy", menuName = "Sunflower/Enemies/Passive Enemy")]
     public class PassiveEnemyData : EnemyData
     {
         [Tooltip("Для Passive: сколько живёт, если его не атакуют")]
         public float passiveLifetime = 5f;
+
+        public override EnemyBehaviorType Behavior => EnemyBehaviorType.Passive;
+
     }
 }
