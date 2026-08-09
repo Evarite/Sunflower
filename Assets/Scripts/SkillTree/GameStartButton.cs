9ea405e85416b89efd2221dc59d2391dd0685dd4
Alaksiej <forestingel@gmail.com>
2026-08-09 16:02:44 +0200

    Player can return to play mode now.

---
+++ Assets/Scripts/SkillTree/GameStartButton.cs
@@ -0,0 +1,24 @@
+﻿using Sunflower.Loading;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Sunflower.SkillTree
+{
+    [RequireComponent(typeof(Button))]
+    [AddComponentMenu("Sunflower/Skill Tree/Game Start Button")]
+    public class GameStartButton : MonoBehaviour
+    {
+        [SerializeField] private SceneAsset _targetScene;
+
+        private Button _button;
+
+        private void Awake() => _button = GetComponent<Button>();
+
+        private void OnEnable() => _button.onClick.AddListener(StartGame);
+
+        private void OnDisable() => _button.onClick.RemoveListener(StartGame);
+
+        private void StartGame() => LoadingScreen.LoadScene(_targetScene.name);
+    }
+}
