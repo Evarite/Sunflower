688a2907599b43193baeea17735538a08082ca4f
Alaksiej <forestingel@gmail.com>
2026-08-10 16:10:42 +0200

    Turret can kill enemies.

--- Assets/Scripts/Enemies/EnemiesSpawnManager.cs
+++ Assets/Scripts/Enemies/EnemiesSpawnManager.cs
@@ -1,11 +1,14 @@
 ﻿using Sunflower.Growth;
 using Sunflower.Utilities.WeightedRandom;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Assets.Scripts.Enemies
 {
     public class EnemiesSpawnManager : MonoBehaviour
     {
+        public static EnemiesSpawnManager Instance { get; private set; }
+
         [SerializeField] private SunflowerGrowth _growth;
 
         [SerializeField] private WeightedRandomList<GameObject> _enemies;
@@ -13,6 +16,21 @@ namespace Assets.Scripts.Enemies
         [SerializeField] private float _spawnDistance = 10f;
 
         [SerializeField] private float _startSpawnHeight = 10f;
+
+        private List<GameObject> _activeEnemies = new();
+
+        public List<GameObject> ActiveEnemies => _activeEnemies;
+
+        public void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+        }
 
         private void Update()
         {
@@ -26,7 +44,10 @@ namespace Assets.Scripts.Enemies
             Vector2 spawn = new Vector2(Random.Range(-1f, 1f), Random.Range(0f, 1f));
             spawn = spawn.normalized * _spawnDistance;
 
-            Instantiate(enemy, new Vector2(0, _growth.Height) + spawn, Quaternion.identity);
+            var spawnedEnemy =
+                Instantiate(enemy, new Vector2(0, _growth.Height) + spawn, Quaternion.identity);
+
+            _activeEnemies.Add(spawnedEnemy);
         }
     }
 }
---
+++ Assets/Scripts/Modules/Modules/Turret/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using Sunflower.Enemies;
+using UnityEngine;
+
+namespace Sunflower.Modules
+{
+    [RequireComponent(typeof(Collider2D))]
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class Bullet : MonoBehaviour
+    {
+        [SerializeField] private float _damage;
+        [SerializeField] private float _lifeTime = 2f;
+        [SerializeField] private float _speed;
+
+        private void Awake() => Destroy(gameObject, _lifeTime);
+
+        private void Update() =>
+            transform.position += transform.rotation * Vector3.right * _speed * Time.deltaTime;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.TryGetComponent<Enemy>(out var enemy))
+            {
+                enemy.TakeDamage(_damage);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
