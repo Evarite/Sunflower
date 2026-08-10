399fc3d5458262952d36590f5feee28cf8a9158f
pixaut <116499977+pixaut@users.noreply.github.com>
2026-08-10 17:34:50 +0300

    asd

--- Assets/Scripts/Modules/Components/NeedComponent.cs
+++ Assets/Scripts/Modules/Components/NeedComponent.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Sunflower.Modifiers;
 using Sunflower.Needs;
 using UnityEngine;
@@ -8,17 +9,40 @@ namespace Sunflower.Modules
     [AddComponentMenu("Sunflower/Module Components/Needs")]
     public class NeedComponent : MonoBehaviour
     {
-
-        [SerializeField] private NeedSystem _needSystem;
-
         public void ApplyModifier(ModifierData modifier, ModuleRuntime moduleRuntime)
+        {
+
+            if (NeedSystem.Instance == null)
+                return;
+
+            NeedSystem.Instance.ApplyModifier(modifier, moduleRuntime);
+        }
+
+        public void ApplyModifiers(List<ModifierData> modifiers, UnityEngine.Object source)
         {
 
             if (_needSystem == null)
                 return;
-            
-            _needSystem.ApplyModifier(modifier, moduleRuntime);
+
+            foreach (ModifierData modifier in modifiers)
+            {
+                _needSystem.ApplyModifier(modifier, source);
+            }
         }
+
+
+        public void ApplyModifiers(List<ModifierData> modifiers, UnityEngine.Object source)
+        {
+
+            if (_needSystem == null)
+                return;
+
+            foreach (ModifierData modifier in modifiers)
+            {
+                _needSystem.ApplyModifier(modifier, source);
+            }
+        }
+
 
         public void RemoveModifiersBySource(ModuleRuntime moduleRuntime)
         {
