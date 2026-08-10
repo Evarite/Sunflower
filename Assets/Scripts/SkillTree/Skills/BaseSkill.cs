86a04565cef43c7026ea1d3e3482c40cccb362af
Alaksiej <forestingel@gmail.com>
2026-08-10 10:56:53 +0200

    BaseSkill created.

--- Assets/Scripts/SkillTree/Skills/BaseSkill.cs
+++ Assets/Scripts/SkillTree/Skills/BaseSkill.cs
@@ -1,4 +1,6 @@
-﻿using Sunflower.SkillTree.Data;
+﻿using Sunflower.Modifiers;
+using Sunflower.Needs;
+using Sunflower.SkillTree.Data;
 using UnityEngine;
 
 namespace Sunflower.SkillTree.Skills
@@ -6,11 +8,16 @@ namespace Sunflower.SkillTree.Skills
     [AddComponentMenu("Sunflower/Skills/Base Skill")]
     public class BaseSkill : Skill
     {
+        [SerializeField] private NeedSystem _needs;
+        [SerializeField] private ModifierData _modifierData;
+
         private void Awake() => Id = SkillId.BaseSkill;
+
+        private void Start() => Ability();
 
         protected override void Ability()
         {
-
+            _needs.ApplyModifier(_modifierData);
         }
     }
 }
